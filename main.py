6a572338cec5013cf535acbf44f14c20f0b0fcdc
nyantama0616 <r_nyantama0616@yahoo.co.jp>
2022-11-24 15:01:37 +0900

    サーバーを立て、動物認識のpostリクエストが来ると、yolov5/n_detect.pyを実行し、結果をclientに返す処理完成

---
+++ main.py
@@ -0,0 +1,40 @@
+from yolov5.n_detect import run
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from urllib import request
+
+import cv2
+import base64
+import numpy as np
+import io
+
+class DectImageRequest(BaseModel):
+    img_base64: str
+    width: int
+    height: int
+    conf: float
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World!!"}
+
+@app.post("/animal-detect")
+async def create_user(request: DectImageRequest):
+    img = decodeImage(request)
+    img_path = "temp/request.jpg"
+    cv2.imwrite(img_path, img)
+
+    detect_result = run(source=img_path, conf_thres=request.conf)
+
+    return {"params": detect_result}
+
+
+def decodeImage(request):
+    img_binary = base64.b64decode(request.img_base64)
+    img = np.frombuffer(img_binary, dtype=np.uint8)
+    img = img.reshape([request.height, request.width, 3])
+
+    return img
